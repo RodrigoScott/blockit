69252f0fddc02f9caa5c72e8548a92ee2dc6b3ac
César Ureño <cesarurenomartinez@gmail.com>
2020-01-03 17:46:49 -0600

    Was configured splash screen for iOS

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jonathan/Documentos/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/jonathan/Documentos/trailock-app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/cesarureno/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cesarureno/flutter/trailock-app"
+export "FLUTTER_TARGET=/Users/cesarureno/flutter/trailock-app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/jonathan/Documentos/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/cesarureno/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
