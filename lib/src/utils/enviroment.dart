e31afc7d1e1b0356b07645105d7fcd929a12d25f
Jonathan Galaviz <54788513+JonathanGalaviz@users.noreply.github.com>
2020-01-11 11:45:27 -0600

    add new client secret (#12)

--- lib/src/utils/enviroment.dart
+++ lib/src/utils/enviroment.dart
@@ -1,10 +1,10 @@
 import 'dart:io';
 
 class Environment {
-  String client_secret = "SoSbXp1xrqPMD4cZVlD5Of8jaId7sIw94hZNjmwG";
+  String client_secret = "tc507xfyD6Y0FkOOvnLMDpl4TAoE2J0zug5hvHPX";
   String client_id = "2";
-  String base_url = 'http://cceo.io:8109/';
-  String base_url_api = 'http://cceo.io:8109/api/v1/';
+  String base_url = 'http://trailock.mx/';
+  String base_url_api = 'http://trailock.mx/api/v1/';
 
   static final Environment _config = new Environment._internal();
 
